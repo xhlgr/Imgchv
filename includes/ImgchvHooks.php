70953ec00ac168f6dc8ae8a889e95b879bb63290
Xhlgr <1850125838@qq.com>
2021-03-10 17:39:05 +0800

    将width、height也加入解析

--- includes/ImgchvHooks.php
+++ includes/ImgchvHooks.php
@@ -16,11 +16,11 @@ class ImgchvHooks {
 	public static function renderImgchv( $input, array $args, Parser $parser, PPFrame $frame ) {
 	    global $wgImgchvDomainName;
 	    $parser->getOutput()->addModules( 'ext.imgchv' );
-        $args['width'] = isset($args['width']) ? htmlspecialchars($args['width']):'100%';
-        $args['height'] = isset($args['height']) ? htmlspecialchars($args['height']):'auto';
+        $inputwidth = $parser->replaceVariables(isset($args['width']) ? htmlspecialchars($args['width']):'100%');
+        $inputheight = $parser->replaceVariables(isset($args['height']) ? htmlspecialchars($args['height']):'auto');
         $inputurl  = $parser->replaceVariables(isset($args['src']) ? htmlspecialchars($args['src']):'', $frame );
         if(substr($inputurl,0,8)!='https://'){//no url
-            $outhtml =  '<div style="position:relative;width:'.$args['width'].';height:'.$args['height'].';"><p>没有图片</p></div>';
+            $outhtml =  '<div style="position:relative;width:'.$inputwidth.';height:'.$inputheight.';"><p>没有图片</p></div>';
         }else{
             $tmpurl = preg_replace('/^(http|https):\/\//', '',$inputurl);
     		$tmpdomain = substr($tmpurl,0,strpos($tmpurl,'/'));
@@ -28,9 +28,9 @@ class ImgchvHooks {
         		$tmp1 = substr($tmpurl,0,strrpos($tmpurl,'.'));
         		$tmp2 = substr($tmpurl,strrpos($tmpurl,'.'));
         		$mdurl = 'https://'.$tmp1.'.md'.$tmp2;
-        		$outhtml =  '<div style="position:relative;width:'.$args['width'].';height:'.$args['height'].';"><img width=100% src="'.$mdurl.'" /><button class="button-showoimg" data-src="'.$inputurl.'" style="position:absolute;top:0;right:0;background-color:white;border-color:blue;color:black;box-shadow: 1px 1px 3px black;" type="button">'.wfMessage("imgchv-button-showoimg")->text().'</button></div>';
+        		$outhtml =  '<div style="position:relative;width:'.$inputwidth.';height:'.$inputheight.';"><img width=100% src="'.$mdurl.'" /><button class="button-showoimg" data-src="'.$inputurl.'" style="position:absolute;top:0;right:0;background-color:white;border-color:blue;color:black;box-shadow: 1px 1px 3px black;" type="button">'.wfMessage("imgchv-button-showoimg")->text().'</button></div>';
     		}else{//url not in array
-    		    $outhtml = '<div style="position:relative;width:'.$args['width'].';height:'.$args['height'].';"><img width=100% src="'.$inputurl.'" /></div>';
+    		    $outhtml = '<div style="position:relative;width:'.$inputwidth.';height:'.$inputheight.';"><img width=100% src="'.$inputurl.'" /></div>';
     		}
         }
         return $outhtml;
